30ef3e04b2fe772f7fb74773b2d789d4acc472cd
Craig Swank <craig.swank@sendgrid.com>
2017-05-04 06:05:19 -0600

    saved custom-face

--- init.el
+++ init.el
@@ -281,7 +281,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "#111111" :foreground "#eeeeec" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Go Mono")))))
 
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
