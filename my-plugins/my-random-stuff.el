000c579f45d5846a0a52f801c27c70e6091b30f4
Craig Swank <craigswank@gmail.com>
2018-06-07 07:27:20 -0600

    added C-cl keybinding for global-display-line-numbers-mode

--- my-plugins/my-random-stuff.el
+++ my-plugins/my-random-stuff.el
@@ -143,6 +143,7 @@
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-cc" 'comment-region)
 (global-set-key "\C-cu" 'uncomment-region)
+(global-set-key "\C-cl" 'global-display-line-numbers-mode)
 (setq-default indent-tabs-mode nil)
 
 ;;; my-random-stuff.el ends here
