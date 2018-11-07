17e0ba48aa4dec9a2e273babf27af7789f1f6363
Craig Swank <craig.swank@sendgrid.com>
2018-11-07 15:20:54 -0700

    more slack niceness

--- my-plugins/my-slack.el
+++ my-plugins/my-slack.el
@@ -7,6 +7,8 @@
 (setq slack-id (getenv "SLACK_CLIENT_ID"))
 (setq slack-secret (getenv "SLACK_CLIENT_SECRET"))
 (setq slack-token (getenv "SLACK_TOKEN"))
+(require 'slack)
+(require 'alert)
 
 (use-package slack
   :commands (slack-start)
@@ -30,35 +32,31 @@
 )
 
 ;; (setq slack-completing-read-function
-;;       #'ido-completing-read)
+
 ;; (setq slack-buffer-function #'switch-to-buffer)
 ;; (setq slack-prefer-current-team t)
-;; (setq slack-display-team-name nil)
+(setq slack-display-team-name nil)
 
 ;; ;;; Go to any channel with `C-x j'.
-;; (define-key ctl-x-map "j" #'slack-select-rooms)
-;; ;;; Quick 'n dirty way of opening the most recent link
-;; ;;; in the current chat room.
-;; (define-key slack-mode-map (kbd "M-o")
-;;   (kbd "<backtab> RET M->"))
-;; ;;; I thumbs-up a lot. Don't judge me.
-;; (define-key slack-mode-map (kbd "C-;") ":+1:")
-;; ;;; Bring up the mentions menu with `@', and insert a
-;; ;;; space afterwards.
-;; (define-key slack-mode-map "@"
-;;   (defun endless/slack-message-embed-mention ()
-;;     (interactive)
-;;     (call-interactively #'slack-message-embed-mention)
-;;     (insert " ")))
+(define-key ctl-x-map "j" #'slack-select-rooms)
+;; ;;; Go to the next buffer that has an unread message.
+(define-key ctl-x-map "n" #'tracking-next-buffer)
 
-;; ;;; Pretty straightforward.
-;; (define-key slack-mode-map (kbd "C-c C-d")
-;;   #'slack-message-delete)
-;; (define-key slack-mode-map (kbd "C-c C-e")
-;;   #'slack-message-edit)
-;; (define-key slack-mode-map (kbd "C-c C-k")
-;;   #'slack-channel-leave)
+(define-key slack-mode-map "@"
+  (defun endless/slack-message-embed-mention ()
+    (interactive)
+    (call-interactively #'slack-message-embed-mention)
+    (insert " ")))
+
+
+(define-key slack-mode-map (kbd "C-c C-d")
+  #'slack-message-delete)
+
+(define-key slack-mode-map (kbd "C-c C-k")
+  #'slack-channel-leave)
+
+(define-key slack-mode-map (kbd "C-c C-e")
+  #'slack-message-edit)
 
 (provide 'my-slack)
 ;;; my-slack.el ends here
- 
