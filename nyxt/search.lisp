4d5fd7834ceb1493464efd13af4356ade5a0502c
Craig Swank <craigswank@gmail.com>
2021-09-11 07:47:43 -0600

    more zsh functions

--- nyxt/search.lisp
+++ nyxt/search.lisp
@@ -6,7 +6,6 @@
        '("gg" "https://www.google.com/search?q=~a" "https://www.google.com/")
        '("go" "https://pkg.go.dev/search?q=~a&m=" "https://golang.org/doc/")))
 
-
 (define-configuration buffer
   ((search-engines (mapcar (lambda (engine)
                              (apply 'make-search-engine engine))
