a1ba516b9c0252dab08c85b50f4a430cf3feb039
Craig Swank <craigswank@gmail.com>
2022-08-14 11:21:01 -0600

    try work vm again

--- nix/files/emacs/my-plugins/my-go.el
+++ nix/files/emacs/my-plugins/my-go.el
@@ -59,33 +59,31 @@
   (dotimes (i (- x 1)) (insert (format "$%d, " (1+ i))))
   (insert (format "$%d" x)))
 
-(defun go-create-playground ()
-  "Create a new temporary file with a skeletal Go application."
-  (interactive)
-  (let ((filename (concat (file-name-as-directory (make-temp-file "go-play-" t)) "main.go")))
-    (find-file filename)
-    (rename-buffer (generate-new-buffer-name "Go Playground"))
-    (insert (concat "package main\n\nimport (\n\t\"fmt\"\n)\n\nfunc main() {\n\tfmt.Println(\"This file is located in " filename "\")\n}"))
-    (save-buffer)
-    (previous-line)
-    (end-of-line)
-    (insert "\n\t")
-    (go-mode)
-    (shell-command "go mod init go-playground")))
+;; (defun go-create-playground ()
+;;   "Create a new temporary file with a skeletal Go application."
+;;   (interactive)
+;;   (let ((filename (concat (file-name-as-directory (make-temp-file "go-play-" t)) "main.go")))
+;;     (find-file filename)
+;;     (rename-buffer (generate-new-buffer-name "Go Playground"))
+;;     (insert (concat "package main\n\nimport (\n\t\"fmt\"\n)\n\nfunc main() {\n\tfmt.Println(\"This file is located in " filename "\")\n}"))
+;;     (save-buffer)
+;;     (previous-line)
+;;     (end-of-line)
+;;     (insert "\n\t")
+;;     (go-mode)
+;;     (shell-command "go mod init go-playground")))
 
+;; (defun go-switch-to-playground ()
+;;   "Switch to Go Playground buffer, creating if necessary."
+;;   (interactive)
+;;   (let ((playground (get-buffer "Go Playground")))
+;;     (if playground
+;;         (switch-to-buffer playground)
+;;       (go-create-playground))))
 
-
-(defun go-switch-to-playground ()
-  "Switch to Go Playground buffer, creating if necessary."
-  (interactive)
-  (let ((playground (get-buffer "Go Playground")))
-    (if playground
-        (switch-to-buffer playground)
-      (go-create-playground))))
-
-(global-set-key (kbd "C-c C-g r") 'go-play-buffer)
-(global-set-key (kbd "C-c C-g p") 'go-switch-to-playground)
-(global-set-key (kbd "C-c C-g n") 'go-create-playground)
+;; (global-set-key (kbd "C-c C-g r") 'go-play-buffer)
+;; (global-set-key (kbd "C-c C-g p") 'go-switch-to-playground)
+;; (global-set-key (kbd "C-c C-g n") 'go-create-playground)
 (global-set-key (kbd "C-c C-g q") 'sql-params)
 
 (provide 'my-go)
