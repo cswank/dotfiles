a883ad2f27fb0a1ffa2ae2ff49058f15abf6e87d
Craig Swank <craigswank@Craigs-MacBook-Pro-2.local>
2024-10-05 15:24:09 -0400

    tweaks to expand-region-twice

--- nix/files/emacs/my-plugins/my-keybindings.el
+++ nix/files/emacs/my-plugins/my-keybindings.el
@@ -9,7 +9,10 @@
 (defun expand-region-twice ()
   "Run `expand-region' then `edit-indirect-region' in sequence."
   (interactive)
-  (er/expand-region 2))
+  (er/expand-region 2)
+  (setq first-char (string (char-after (region-beginning))))
+  (if (eq first-char "`")
+    (er/contract-region 1)))
 
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-cc" 'comment-region)
