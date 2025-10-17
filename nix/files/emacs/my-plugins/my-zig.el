ce431ddb40c1f1c822ec3f29fa2d93469f66e12d
Craig Swank <craigswank@gmail.com>
2025-10-17 07:28:48 -0600

    disable eglot inlay hints

--- nix/files/emacs/my-plugins/my-zig.el
+++ nix/files/emacs/my-plugins/my-zig.el
@@ -1,8 +1,10 @@
-;;; my-c --- Summary
-;;; setup c development
+;;; my-zig --- Summary
+;;; setup zig development
 ;;; Commentary:
 
 (use-package emacs
   :hook (zig-mode . eglot-ensure))
 
 (add-hook 'zig-mode-hook 'company-mode)
+(add-hook 'eglot-managed-mode-hook
+          (lambda () (eglot-inlay-hints-mode -1)))
